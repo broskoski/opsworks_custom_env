38351bb9374618d9856417401bd482e3da980827
Stefan Wrobel <swrobel@server.fake>
2014-01-09 18:50:21 -0800

    Move rails-app check to write_config recipe

--- recipes/write_config.rb
+++ recipes/write_config.rb
@@ -2,11 +2,15 @@
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
 
 node[:deploy].each do |application, deploy|
-  
+
   custom_env_template do
     application application
     deploy deploy
     env node[:custom_env][application]
+
+    if node[:opsworks][:instance][:layers].include?('rails-app')
+      notifies :run, resources(:execute => "restart Rails app #{application} for custom env")
+    end
   end
-  
+
 end
